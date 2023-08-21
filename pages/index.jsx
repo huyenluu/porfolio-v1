22bcab843bce2920390a2d97ce2a47d0de6ce891
Huyen Luu <50383878+huyenluu@users.noreply.github.com>
2023-08-21 23:30:07 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,15 +3,21 @@ import Contact from './contact/contact';
 import Footer from './footer/footer';
 import Hero from './hero/hero';
 import Project from './projects/projects';
+import Head from "next/head";
 
 export default function Home() {
     return (
-        <main className="bg-black bg-cover text-white">
-            <Hero />
-            <Project />
-            <About />
-            <Contact />
-            <Footer />
-        </main>
+        <>
+            <Head>
+                <title>Huyen Luu - Front end Developer</title>
+            </Head>
+            <main className="bg-black bg-cover text-white">
+                <Hero />
+                <Project />
+                <About />
+                <Contact />
+                <Footer />
+            </main>
+        </>
     );
 }
